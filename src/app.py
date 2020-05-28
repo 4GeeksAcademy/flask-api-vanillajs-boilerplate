15bcc74a9c54d868bc5b7511b687f1769942c11d
Alejandro Sanchez <aalejo@gmail.com>
2020-05-28 19:26:59 +0000

    trying to publish to heroku

--- src/app.py
+++ src/app.py
@@ -15,12 +15,18 @@ DEBUG = os.getenv("FLASK_ENV") == "development"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
-db.init_app(app)
+
+# database condiguration
+if os.getenv("DB_CONNECTION_STRING") is not None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    MIGRATE = Migrate(app, db)
+    db.init_app(app)
+
+# Allow CORS requests to this API
 CORS(app)
 
+# Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
